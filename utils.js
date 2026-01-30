1013f84543327f827f0e1235af77de3686443811
physarif <phys.arif@gmail.com>
2026-01-30 20:50:11 +0600

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,1 +1,17 @@
-.
+// ----- Bangla Number -----
+export function toBN(num = 0) {
+  const map = ['০','১','২','৩','৪','৫','৬','৭','৮','৯'];
+  return num.toString().replace(/\d/g, d => map[d]);
+}
+
+// ----- Time Format -----
+export function formatTime(ts) {
+  if (!ts) return '';
+  const d = ts.toDate();
+  return d.toLocaleString('bn-BD', {
+    hour: '2-digit',
+    minute: '2-digit',
+    day: 'numeric',
+    month: 'short'
+  });
+}
