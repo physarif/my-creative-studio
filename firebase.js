4053b5e047f99760b980f7cb851dc6ddd7a454bf
physarif <phys.arif@gmail.com>
2026-01-30 20:52:03 +0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,26 +1,25 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-firestore.js";
 import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-firestore.js";
 
+// ----- CONFIG -----
 const firebaseConfig = {
-  apiKey: "AIzaSyB_xkcJ87-E4jkYIDT2dBxOFJHUQvZxbU8",
-  authDomain: "myblogcomments-22808.firebaseapp.com",
-  projectId: "myblogcomments-22808",
-  storageBucket: "myblogcomments-22808.firebasestorage.app",
-  messagingSenderId: "1067155293144",
-  appId: "1:1067155293144:web:8f10b016462b52d36628ad"
+  apiKey: "YOUR_KEY",
+  authDomain: "YOUR_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_BUCKET",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
 };
 
-// Init
+// ----- INIT -----
 export const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
 export const auth = getAuth(app);
+export const db   = getFirestore(app);
 export const provider = new GoogleAuthProvider();
 
-// Paths
-export const POSTS_PATH = 'blog_comments/pages/forum_v2_voted';
-export const REPORTS_PATH = 'blog_comments/pages/reports';
-export const BOOKMARKS_PATH = 'blog_comments/pages/bookmarks';
-
-// Admin
-export const ADMIN_EMAIL = "phys.arif@gmail.com";
+// ----- CONST -----
+export const ADMIN_EMAIL   = "admin@gmail.com";
+export const POSTS_PATH    = "posts";
+export const REPORTS_PATH  = "reports";
+export const BOOKMARKS_PATH= "bookmarks";
